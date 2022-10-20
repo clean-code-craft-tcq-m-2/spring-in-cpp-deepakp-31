6e11ab4625b19cca918c3dfbccfac2a6a09991a5
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-10-20 15:44:30 +0530

    Update stats.h
    Added struct

--- stats.h
+++ stats.h
@@ -1,6 +1,13 @@
 #include <vector>
 
 namespace Statistics {
+    
+    struct Stats{
+        float average;
+        float max;
+        float min;
+    };
+    
     Stats ComputeStatistics(const std::vector<___>& );  // Structure need to be return type.
                                                         //three variables in struct - average  max & min
 }
