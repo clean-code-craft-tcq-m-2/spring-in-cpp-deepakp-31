f9ab3cefa436f13aff1277dc78479c6df5520ef6
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-10-20 12:03:05 +0530

    Update stats.h
    Comments for variable needed

--- stats.h
+++ stats.h
@@ -1,5 +1,6 @@
 #include <vector>
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<___>& );  // Structure need to be return type.
+                                                        //three variables in struct - average  max & min
 }
