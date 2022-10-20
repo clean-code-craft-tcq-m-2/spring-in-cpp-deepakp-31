67caa4bcd48eee2f882f86f604ce3fe98a3f08ff
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-10-20 17:05:45 +0530

    Update stats.cpp
    added namespace in struct

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,5 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
     //Implement statistics here
 }
