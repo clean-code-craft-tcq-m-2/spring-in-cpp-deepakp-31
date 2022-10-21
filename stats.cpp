35a4bf18e03a419e2d295b18958da79a300526af
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-10-21 10:48:29 +0530

    Update stats.cpp
    Added average, max and min computation

--- stats.cpp
+++ stats.cpp
@@ -1,8 +1,29 @@
 #include "stats.h"
 
-Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
     //Implement statistics here
     Statistics::Stats Stat{0.0, 0.0, 0.0};
     
+    float temp_min = numbers.at(0);
+    float temp_max = numbers.at(0);
+    float sum =numbers.at(0);
+    for(int i =1; i< numbers.size(); i++)
+    {
+        if (temp_max < numbers.at(i))
+        {
+             temp_max=   numbers.at(i);
+        }
+        if (temp_min > numbers.at(i))
+        {
+             temp_min=   numbers.at(i);
+        }
+        
+        sum = sum + numbers.at(i);
+            
+    }
+    Stat.average =sum /numbers.size();
+    Stat.max = temp_max;
+    Stat.min = temp_min;
+        
     return Stat;
 }
