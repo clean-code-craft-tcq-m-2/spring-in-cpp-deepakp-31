24901d9644f1ea87d517ebb02b7af0a2236fcf3d
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-10-21 15:41:19 +0530

    Update stats.cpp
    Checking the size of vector passed and updating the struct with NaN

--- stats.cpp
+++ stats.cpp
@@ -1,29 +1,39 @@
 #include "stats.h"
 
-Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
-    //Implement statistics here
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float> &numbers)
+{
+    // Implement statistics here
     Statistics::Stats Stat{std::nanf("0.0"), std::nanf("0.0"), std::nanf("0.0")};
-    
-    float temp_min = numbers.at(0);
-    float temp_max = numbers.at(0);
-    float sum =numbers.at(0);
-    for(long unsigned int i = 1; i< numbers.size(); i++)
-    {
-        if (temp_max < numbers.at(i))
+
+    if ((numbers.size()==0)
         {
-             temp_max=   numbers.at(i);
-        }
-        if (temp_min > numbers.at(i))
-        {
-             temp_min=   numbers.at(i);
+        Stat.average = std::nanf("0.0");
+        Stat.max = std::nanf("0.0");
+        Stat.min = std::nanf("0.0");   
         }
         
-        sum = sum + numbers.at(i);
-            
+    else{
+        float temp_min = numbers.at(0);
+        float temp_max = numbers.at(0);
+        float sum = numbers.at(0);
+        for (long unsigned int i = 1; i < numbers.size(); i++)
+        {
+            if (temp_max < numbers.at(i))
+            {
+                temp_max = numbers.at(i);
+            }
+            if (temp_min > numbers.at(i))
+            {
+                temp_min = numbers.at(i);
+            }
+
+            sum = sum + numbers.at(i);
+        }
+        Stat.average = sum / numbers.size();
+        Stat.max = temp_max;
+        Stat.min = temp_min;
+        
     }
-    Stat.average =sum /numbers.size();
-    Stat.max = temp_max;
-    Stat.min = temp_min;
         
     return Stat;
 }
