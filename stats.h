1c63250761f31476fb4070c302ea894e62ef5c7b
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-10-22 20:07:01 +0530

    Update stats.h
    Added Alert namespace

--- stats.h
+++ stats.h
@@ -12,3 +12,65 @@ namespace Statistics {
     
     Stats ComputeStatistics(const std::vector<float>& numbers); 
 }
+
+namespace Alert
+{
+    //Base class IAlerter
+    class IAlerter
+    {
+    public:
+        virtual void alert();
+    };
+
+    // Email
+    class EmailAlert : public IAlerter
+    {
+    public:
+        bool emailSent;
+
+        EmailAlert()
+        {
+            emailSent = false;
+        }
+
+        void alert(){
+            emailSent= true;
+        }
+    };
+
+    // LED
+    class LEDAlert : public IAlerter
+    {
+    public:
+        bool ledGlows;
+
+        LEDAlert()
+        {
+            ledGlows = false;
+        }
+
+        void alert(){
+            ledGlows= true;
+        }
+    };
+
+    class StatsAlerter
+    {
+
+    private:
+        float mThreshold;
+        std::vector<IAlerter *> alerts;
+
+    public:
+        // Constructor
+        StatsAlerter(const float maxThreshold, std::vector<IAlerter *> alerters)
+        {
+            mThreshold = maxThreshold;
+            alerts = alerters;
+        }
+
+        // method checkAndAlert
+        void checkAndAlert(const std::vector<float> &numbers);
+        
+    };
+}
